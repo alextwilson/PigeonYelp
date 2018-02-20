1f9d469bb70c6c0396c4627e96a5ff0136909f68
alextwilson <github@alextwilson.co.uk>
2018-02-20 10:12:49 +0000

    Database issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219172348) do
+ActiveRecord::Schema.define(version: 20180219184156) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating", limit: 5
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
