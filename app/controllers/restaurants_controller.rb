02d34faadc64b9c14233a646bc17127c1b6db0b4
Simon Bao <Simonbao.sb@gmail.com>
2018-02-20 11:29:46 +0000

    Cleaned up restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,49 +1,54 @@
 class RestaurantsController < ApplicationController
-  def index
-    @restaurants = Restaurant.all
-  end
+	def index
+		@restaurants = Restaurant.all
+	end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
+	def show
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant_average = average_rating(@restaurant)
+	end
 
-  def new
-    @restaurant = Restaurant.new
-  end
+	def new
+		@restaurant = Restaurant.new
+	end
 
-  def edit
-    @restaurant = Restaurant.find(params[:id])
-  end
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
 
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
+	def create
+		@restaurant = Restaurant.new(restaurant_params)
 
-    if @restaurant.save
-      redirect_to @restaurant
-    else
-      render 'new'
-    end
-  end
+		if @restaurant.save
+			redirect_to @restaurant
+		else
+			render 'new'
+		end
+	end
 
-  def update
-    @restaurant = Restaurant.find(params[:id])
+	def update
+		@restaurant = Restaurant.find(params[:id])
 
-    if @restaurant.update(restaurant_params)
-      redirect_to @restaurant
-    else
-      render 'edit'
-    end
-  end
+		if @restaurant.update(restaurant_params)
+			redirect_to @restaurant
+		else
+			render 'edit'
+		end
+	end
 
-  def destroy
-    @restaurant = Restaurant.find(params[:id])
-    @restaurant.destroy
+	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.destroy
 
-    redirect_to restaurants_path
-  end
+		redirect_to restaurants_path
+	end
 
-  private
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :description)
-    end
+	private
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :description)
+	end
+
+	def average_rating(restaurant)
+		restaurant.reviews.average(:rating).round(2)
+	end
 end
