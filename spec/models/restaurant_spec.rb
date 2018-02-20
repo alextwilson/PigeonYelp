4c5e82c46cf023fe98ee7cec9c8e3a7ed8ee225f
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-20 16:49:43 +0000

    Add devise integration, User's cant delete or edit restaurants not owned by them

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,10 +5,4 @@ RSpec.describe Restaurant do
     restaurant = Restaurant.new(name: nil, description: "This is my restaurant")
     expect(restaurant).to_not be_valid
   end
- 
-
-  it "create restaurant with a description" do
-    restaurant = Restaurant.new(name: 'test', description: "My restaurant")
-    expect(restaurant).to be_valid
-  end
 end
