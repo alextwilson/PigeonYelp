8f3b47d1e7b762754919fa2a37f7de5aaf2e6de0
alextwilson <github@alextwilson.co.uk>
2018-02-21 16:32:52 +0000

    fixes merge again

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -6,10 +6,7 @@ feature 'Reviews' do
       click_link 'Show'
       expect(page).not_to have_content 'Add a review:'
     end
-<<<<<<< HEAD
-=======
 
->>>>>>> 7b1b5de2a91a63d250b87edb15eb6a4b2aaaebac
     scenario 'user can delete their own reviews' do
       user_login('alex@gmail.com')
       create_restaurant
@@ -24,8 +21,6 @@ feature 'Reviews' do
       click_link 'Delete'
       expect(page).not_to have_content 'Really Bad'
     end
-<<<<<<< HEAD
-=======
 
     scenario 'user cant delete other users reviews' do
       user_login('alex@gmail.com')
@@ -40,10 +35,9 @@ feature 'Reviews' do
       click_button 'Create Review'
       click_link 'Logout'
       user_login('alex@gmail.com')
-      click_link 'All restaurants'  
+      click_link 'All restaurants'
       click_link 'Show'
       expect(page).not_to have_content 'Delete'
     end
->>>>>>> 7b1b5de2a91a63d250b87edb15eb6a4b2aaaebac
   end
 end
