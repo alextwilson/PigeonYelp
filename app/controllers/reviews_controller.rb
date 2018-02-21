20c8ee7320727b2b16c4c86781891fc42515fef5
Alexandra Fina <alexandra.fina@yahoo.com>
2018-02-21 18:11:53 +0000

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,11 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.create(review_params)
+    
     redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
-    p params
     @review = Review.find(params[:restaurant_id])
     @review.destroy
 
