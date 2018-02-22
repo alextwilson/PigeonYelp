18e9866ff52237a0be2ad819e25965e723b394ef
Simon Bao <Simonbao.sb@gmail.com>
2018-02-22 17:45:45 +0000

    Added aws upload

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,1 +1,20 @@
 require 'carrierwave/orm/activerecord'
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'ADD_KEY',
+      aws_secret_access_key: 'ADD_ACCESS_KEY',
+      region: 'eu-west-2'
+  }
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'pigeonyelp'
+      config.asset_host = 'https://s3-eu-west-2.amazonaws.com/pigeonyelp'
+
+    when 'development'
+      config.fog_directory = 'pigeonyelp'
+      config.asset_host = 'https://s3-eu-west-2.amazonaws.com/pigeonyelp'
+
+  end
+end
