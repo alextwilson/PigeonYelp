f7174c8e8008044e13b5446a83cf7a0262c21c18
Simon Bao <Simonbao.sb@gmail.com>
2018-02-22 14:28:24 +0000

    Restaurant attribute tests

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant do
+  subject(:restaurant) {described_class.new(name: "PizzaTown", description: "We sell Chinese food!")}
+
   it "restaurant is not valid without a title" do
     restaurant = Restaurant.new(name: nil, description: "This is my restaurant")
     expect(restaurant).to_not be_valid
+  end
+
+  context "Successful resturant creation" do
+    it "has a name" do
+      expect(restaurant.name).to eq("PizzaTown")
+    end
+
+    it "has a description" do
+      expect(restaurant.description).to eq("We sell Chinese food!")
+    end
   end
 end
